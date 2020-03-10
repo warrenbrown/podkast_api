47146f5a6fae5606165049f7d90210fee91ee989
Warren Brown <warren@katapultmedia.com>
2020-03-10 09:48:02 -0500

    chore: setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
       resources :users, only: [ :show, :create, :update, :destroy ]
       resources :podcasts
       resources :episodes
+      resources :tokens, only: [ :create ]
     end
   end
 end
